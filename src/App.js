93184d6d6a448ca1535c87705a013eeb54fb8494
Igor Djuric <igordjuric@Igors-MacBook-Air.local>
2020-12-15 22:52:09 +0100

    First start

--- src/App.js
+++ src/App.js
@@ -2,13 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const variable = ["Bla", "bla", "truc"];
+  const mina = [10, 15, 20, 30, 40];
+
+  let output = "";
+
+  for (let i =0; i< mina.length;i++) {
+    output += mina[i]+ " ";
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="WelcomeMessage">
+          Hello & Welcome to our Unique world of jewelry! Let us take you on a jurney through the cristals...
         </p>
+        <ul className="Selection">
+          {variable.map (function (item) {
+            return (
+              <li>
+                {item}
+                </li>
+
+            )
+          }
+          )}
+          </ul>
+          
+          {mina.map (function(item) {
+            return (
+              <span>{item}</span>
+            )
+          }
+          )}
+         
+
+          {variable}
+          <br/>
+          {output}
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +49,7 @@ function App() {
         >
           Learn React
         </a>
+    
       </header>
     </div>
   );
